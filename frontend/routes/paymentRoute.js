0a704d0c826857bf66e84ff7ab629b9b9fa9e598
Nitish <ns5491415@gmail.com>
2026-06-07 14:37:40 +0530

    added fronted

--- frontend/routes/paymentRoute.js
+++
@@ -1,8 +0,0 @@
-const express=require("express");
-const router=express.Router();
-const {createdOrder,verifyPayment}=require("../controllers/paymentController");
-
-router.post("/order",createdOrder);
-router.post("/verify",verifyPayment);
-
-module.exports=router;
